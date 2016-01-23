170a06c6a4b19d795e914de5931ccd422449e722
rurarz <olekrurarz@gmail.com>
2016-01-23 22:17:11 +0100

    Sinus computing add

--- x64.asm
+++ x64.asm
@@ -2,6 +2,8 @@
 ; Some daa over here
 section .data
 
+six: dd 6.0
+zero: dd 0.0
 one: dd 1.0
 maxByte: dd 256.0
 
@@ -66,15 +68,58 @@ _xd:
 	;prepare registers
 	;used registers RCX, RDX, R11, R12
 
-	mov r11d, 0
-	mov r12d, 0
-	sub r12, 1
+	mov r11, 0
+	mov r12, 0
+	;sub r12, 1
 
 
 color_row:
 	;call blending
 	;BGR - color send
 	;need three registers for saving color
+
+	;---------SINUS-------------------
+  ;X
+	cvtsi2ss xmm0, r11
+	;Xc
+	mov eax, clickedX
+	cvtsi2ss xmm1, eax
+	;Y
+	cvtsi2ss xmm2, r12
+	;Yc
+	mov eax, clickedY
+	cvtsi2ss xmm3, eax
+	;X- Xc
+	subss xmm0, xmm1
+	;Y-Yc
+	subss xmm2, xmm3
+	;power 2
+	mulss xmm0, xmm0
+	mulss xmm2, xmm2
+	;Xw + Yw
+	addss xmm0, xmm2
+	sqrtss  xmm0, xmm0
+	; dividing x by imageWidth
+	mov eax, imageWidth
+	cvtsi2ss xmm2, eax
+	divss xmm0, xmm2
+
+	;---------SINUS-----------------
+	;x - x^3/6
+	;save x
+
+	movss xmm1, [zero]
+	subss xmm1, xmm0
+	mulss xmm1, xmm0
+	mulss xmm1, xmm0
+	divss xmm1, [six]
+	addss xmm1, xmm0
+
+	; XMM1 <--- Our sinus !!!!
+
+
+	mov r8b, [imagePointer]
+	mov r9b, [secondImagePointer]
 	jmp blendB
 
 endBlendingB:
@@ -83,16 +128,21 @@ endBlendingB:
 	;write B to image pixel
 	mov byte [imagePointer], bl
 	;---------G----------------
-	mov  bl, [secondImagePointer + 1]
+	inc secondImagePointer
+	mov  bl, [secondImagePointer]
 	;write G to image pixel
-	mov byte [imagePointer + 1], bl
-	mov  bl, [secondImagePointer + 2]
+	inc imagePointer
+	mov byte [imagePointer], bl
+
+	inc secondImagePointer
+	mov  bl, [secondImagePointer]
 	;write R to image pixel
-	mov byte [imagePointer +2 ], bl
+	inc imagePointer
+	mov byte [imagePointer], bl
 
 
-	add imagePointer, 4
-	add secondImagePointer, 4
+	add imagePointer, 2
+	add secondImagePointer, 2
 	inc r11
 	;--------
 	mov eax, imageWidth
